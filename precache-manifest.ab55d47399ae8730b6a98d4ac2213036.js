88f1245fbe8380e7a0db981bd3f6ae66da47e445
AjeyakumarTM <ajeyakumartmegadi@gmail.com>
2020-08-17 18:07:33 +0530

    Updates

---
+++ precache-manifest.ab55d47399ae8730b6a98d4ac2213036.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0885074682b2e7e5adcec45ee8f552e4",
+    "url": "/react-app-deploy1/index.html"
+  },
+  {
+    "revision": "dae3f8160eeecc9e68fc",
+    "url": "/react-app-deploy1/static/js/2.3f76aa6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-deploy1/static/js/2.3f76aa6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79cdc5b0ad8fa10157b",
+    "url": "/react-app-deploy1/static/js/main.265ed3d5.chunk.js"
+  },
+  {
+    "revision": "5a6e711150c962d95d11",
+    "url": "/react-app-deploy1/static/js/runtime-main.7cafbbcf.js"
+  }
+]);
